7cda733fae4d729ba4c7e33d497af18a66559e18
vikasRanjann <vikash.ranjan933.com>
2023-07-31 16:34:55 +0530

    comment add

--- routes/note.js
+++ routes/note.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (response, request) => {
+  console.log("Insided note");
+
+  response.setEncoding("hi");
+});
+
 module.exports = router;
